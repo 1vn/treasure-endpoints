c474724cca48e0be6e7f9b6d0fd6240b4a4965b4
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-09-19 21:59:01 -0400

    omg

--- icl/newitem.inc.php
+++ icl/newitem.inc.php
@@ -5,14 +5,12 @@ function newitem(){
 	$name=$_POST['name'];
 	$price=$_POST['price'];
 	$userid=$_POST['userid'];
-	$image64=$_POST['image'];
-
-	print_r($_POST);
+	$image=$_POST['image'];
 
 	if($_POST['tags']){
 		$tags=explode(',', $_POST['tags']);
 	}
-	$query="insert into useritems (name, price, userid, image) values ('$name', $price, $userid, '$image64')";
+	$query="insert into useritems (name, price, userid, image) values ('$name', $price, $userid, '$image')";
 	$rs=sql_query($query, $db);
 	$useritemid=sql_insert_id($db, $rs);
 
