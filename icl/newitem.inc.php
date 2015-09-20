8e5e3cc2f6bb11c97a736167f202952e283e1199
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-09-19 21:59:01 -0400

    its late

--- icl/newitem.inc.php
+++ icl/newitem.inc.php
@@ -28,24 +28,20 @@ function newitem(){
 			$file_name=$_FILES["files"]["name"][$key];
 			$file_tmp=$_FILES['files']['tmp_name'][$key];
 			$ext=pathinfo($file_name,PATHINFO_EXTENSION);
-			if(in_array($ext,$extension)){
-				if(!file_exists("itemimages/".$txtGalleryName."/".$file_name)){
-                    $newFileName=$useritemid.".".$ext;
-                    move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"itemimages/".$newFileName);
-               		$query="insert into itemimages (useritemid, filename) values ($useritemid, '$newFileName')";
-  					echo "2";
-               		$rs=sql_query($query, $db);
-                } else{
-                    $filename=basename($file_name,$ext);
-                    $newFileName=$useritemid.time().".".$ext;
-                    move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"itemimages/".$newFileName);
-                	$query="update itemimages set status=0 where useritemid=useritemid and status=1";
-                	$rs=sql_query($query, $db);
-                	$query="insert into itemimages (useritemid, filename) values ($useritemid, '$newFileName')";
-                	$rs=sql_query($query, $db);
-  					echo "3";
-                }
-			}
+			if(!file_exists("itemimages/".$txtGalleryName."/".$file_name)){
+                $newFileName=$useritemid.".".$ext;
+                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"itemimages/".$newFileName);
+           		$query="insert into itemimages (useritemid, filename) values ($useritemid, '$newFileName')";
+           		$rs=sql_query($query, $db);
+            } else{
+                $filename=basename($file_name,$ext);
+                $newFileName=$useritemid.time().".".$ext;
+                move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"itemimages/".$newFileName);
+            	$query="update itemimages set status=0 where useritemid=useritemid and status=1";
+            	$rs=sql_query($query, $db);
+            	$query="insert into itemimages (useritemid, filename) values ($useritemid, '$newFileName')";
+            	$rs=sql_query($query, $db);
+            }
 		}
 	}
 }
