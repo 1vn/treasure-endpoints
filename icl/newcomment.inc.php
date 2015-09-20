9e78ffefc36b20984590709b594de67ab486ac87
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-09-20 04:09:22 -0400

    oops

--- icl/newcomment.inc.php
+++ icl/newcomment.inc.php
@@ -2,12 +2,12 @@
 
 function newcomment(){
 	global $db;
-	$useritemid=$_POST['useritemid'];
 	$userid=$_POST['userid'];
+	$commenterid=$_POST['commenterid'];
 	$created_at=time();
 	$comment=$_POST['comment'];
 
-	$query="insert into userprofilecomments (useritemid, userid, comment, created_at) values ($useritemid, $userid, '$comment', $created_at)";
+	$query="insert into userprofilecomments (userid, commenterid, comment, created_at) values ($userid, $commenterid, '$comment', $created_at)";
 	$rs=sql_query($query, $db);
 	echo json_encode(array('success'=>true));
 }
